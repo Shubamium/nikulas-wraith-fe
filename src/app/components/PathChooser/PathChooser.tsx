f162c3e1617626db29348323a01f1ebe6c4c5209
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-05-22 13:48:19 +0700

    Fix counter and change art

--- src/app/components/PathChooser/PathChooser.tsx
+++ src/app/components/PathChooser/PathChooser.tsx
@@ -1,89 +1,119 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import './pathChooser.scss'
-import { updatePath } from '@/db/serverActions'
+"use client";
+import React, { useEffect, useState } from "react";
+import "./pathChooser.scss";
+import { updatePath } from "@/db/serverActions";
 type Props = {
-	onPathChosen: () => void
-	bad:number,
-	good:number,
-}
+  onPathChosen: () => void;
+  bad: number;
+  good: number;
+};
 
-export default function PathChooser({onPathChosen,bad,good}: Props) {
-	const [pathChoosen, setPathChoosen] = useState(false)
-	const [done,setDone] = useState(false)
-	const [slocalStorage, setLocalStorage] = useState<any>()
-	const [badCount,setBadCount ]= useState(bad)
-	const [goodCount, setGoodCount] = useState(good)
-	const total = badCount + goodCount
-	const badPercentage = badCount / total * 100
-	const goodPercentage = goodCount / total * 100
-	
+export default function PathChooser({ onPathChosen, bad, good }: Props) {
+  const [pathChoosen, setPathChoosen] = useState(false);
+  const [done, setDone] = useState(false);
+  const [slocalStorage, setLocalStorage] = useState<any>();
+  const [badCount, setBadCount] = useState(bad);
+  const [goodCount, setGoodCount] = useState(good);
+  const total = badCount + goodCount;
+  const badPercentage = (badCount / total) * 100;
+  const goodPercentage = (goodCount / total) * 100;
 
-	const pathChoosenHandler = (path:string) => {
-		localStorage.setItem('hasChoosenPath', 'true')
-		setPathChoosen(true)
-		setBadCount(badCount + (path === 'bad' ? 1 : 0))
-		setGoodCount(goodCount + (path === 'good' ? 1 : 0))
-		updatePath(path,goodCount,badCount)
-	}
-	useEffect(()=>{
-		// setLocalStorage(localStorage)
-		if(pathChoosen){
-			setTimeout(()=>{
-				onPathChosen && onPathChosen()
-				setDone(true)
-			},5000)
-		}
-	},[pathChoosen])
+  const pathChoosenHandler = (path: string) => {
+    localStorage.setItem("hasChoosenPath", "true");
+    setPathChoosen(true);
+    setBadCount(badCount + (path === "bad" ? 1 : 0));
+    setGoodCount(goodCount + (path === "good" ? 1 : 0));
+    updatePath(path, goodCount, badCount);
+  };
+  useEffect(() => {
+    // setLocalStorage(localStorage)
+    if (pathChoosen) {
+      setTimeout(() => {
+        onPathChosen && onPathChosen();
+        setDone(true);
+      }, 5000);
+    }
+  }, [pathChoosen]);
 
-	
-	return (
-	<div className={`thepaths ${done ? 'done' : ''}`}>
-		<div className={`path-container ${pathChoosen ? 'choosen' : ''}`}>
-			<div className="path-picker">
-			<div className="title">
-					<h2>Welcome To The <b>Phantom Realm</b>, Spirit!</h2>
-					<p>A choice for you to make before you enter.</p>
-				</div>
-				<div className="paths">
-					
-					<div className="path good">
-						<div className='path-button' onClick={()=>{pathChoosenHandler('good')}}>
-							<svg xmlns="http://www.w3.org/2000/svg" width="136" height="136" viewBox="0 0 136 136" fill="none">
-								<circle cx="68" cy="68" r="49" fill="#284E6F"/>
-								<circle cx="68" cy="68" r="67.5" stroke="#284E6F"/>
-							</svg>
-							<p>Do you take <br/> the path of good?</p>
-						</div>
-					</div>
+  return (
+    <div className={`thepaths ${done ? "done" : ""}`}>
+      <div className={`path-container ${pathChoosen ? "choosen" : ""}`}>
+        <div className="path-picker">
+          <div className="title">
+            <h2>
+              Welcome To The <b>Phantom Realm</b>, Spirit!
+            </h2>
+            <p>A choice for you to make before you enter.</p>
+          </div>
+          <div className="paths">
+            <div className="path good">
+              <div
+                className="path-button"
+                onClick={() => {
+                  pathChoosenHandler("good");
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="136"
+                  height="136"
+                  viewBox="0 0 136 136"
+                  fill="none"
+                >
+                  <circle cx="68" cy="68" r="49" fill="#284E6F" />
+                  <circle cx="68" cy="68" r="67.5" stroke="#284E6F" />
+                </svg>
+                <p>
+                  Do you take <br /> the path of good?
+                </p>
+              </div>
+            </div>
 
-					<div className="path corruption" onClick={()=>{pathChoosenHandler('bad')}}>
-						<div className='path-button'>
-							<p>Or the path  <br/>  of C̸̹̋ȏ̸̧r̶̺̀r̸̛͚u̶̹͗p̴̭͝t̶̨̐į̸͆ó̷̥ǹ̶̥?</p>
-							<svg xmlns="http://www.w3.org/2000/svg" width="136" height="136" viewBox="0 0 136 136" fill="none">
-								<circle cx="68" cy="68" r="49" fill="#284E6F"/>
-								<circle cx="68" cy="68" r="67.5" stroke="#284E6F"/>
-							</svg>
-						</div>
-					</div>
-					
-				</div>
-				<div className="path-art">
-					<img src="art/nik.png" alt="" className='nik good-nik' />
-					<img src="art/cor_nik.png" alt="" className='nik bad-nik' />
-				</div>
-			</div>
-		</div>
-		<div className={`path-result ${pathChoosen ? 'choosen' : ''}`}>
-		<img src="decor/arrow_small.png" alt="" className='arrow' />
-			<p className='good'>{goodCount}</p>
-			<div className="bar">
-				<div className="good-fill" style={pathChoosen ? {width:''+goodPercentage+'%'} : {}}></div>
-				<div className="cor-fill" style={pathChoosen ? {width:''+badPercentage+'%'} : {}}></div>
-			</div>
-			<p className='corrupt'>{badCount}</p>
-		<img src="decor/arrow_small.png" alt="" className='arrow reverse'/>
-		</div>
-	</div>
-	)
+            <div
+              className="path corruption"
+              onClick={() => {
+                pathChoosenHandler("bad");
+              }}
+            >
+              <div className="path-button">
+                <p>
+                  Or the path <br /> of C̸̹̋ȏ̸̧r̶̺̀r̸̛͚u̶̹͗p̴̭͝t̶̨̐į̸͆ó̷̥ǹ̶̥?
+                </p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="136"
+                  height="136"
+                  viewBox="0 0 136 136"
+                  fill="none"
+                >
+                  <circle cx="68" cy="68" r="49" fill="#284E6F" />
+                  <circle cx="68" cy="68" r="67.5" stroke="#284E6F" />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="path-art">
+            <img src="art/nik.png" alt="" className="nik good-nik" />
+            <img src="art/cor_nik.png" alt="" className="nik bad-nik" />
+          </div>
+        </div>
+      </div>
+      <div className={`path-result ${pathChoosen ? "choosen" : ""}`}>
+        <img src="decor/arrow_small.png" alt="" className="arrow" />
+        <p className="good">{goodCount}</p>
+        <div className="bar">
+          <div
+            className="good-fill"
+            style={pathChoosen ? { width: "" + goodPercentage + "%" } : {}}
+          ></div>
+          <div
+            className="cor-fill"
+            style={pathChoosen ? { width: "" + badPercentage + "%" } : {}}
+          ></div>
+        </div>
+        <p className="corrupt">{badCount}</p>
+        <img src="decor/arrow_small.png" alt="" className="arrow reverse" />
+      </div>
+    </div>
+  );
 }
--- src/app/section/HeroSection/HeroSection.tsx
+++ src/app/section/HeroSection/HeroSection.tsx
@@ -1,65 +1,126 @@
-import 'react'
-import SectionTitle from '@/app/components/SectionTitle/SectionTitle'
-import './heroSection.scss'
-import Link from 'next/link'
-export default function HeroSection({bad,good,activeNav}: {bad?:number,good?:number,activeNav:string}) {
-	return (
-		<section id="container_hero">
-			<h1 style={{display:'none'}}>Nikulas Wraith</h1>
-			<div className='path-counter'>
-			{bad && good && 	<p> {'>>>'} <span className='good'>{good}</span>-<span className='bad'>{bad}</span>{'<<<'}</p>}
-			</div>
-			<div className="display" id='top'>
-				<div className="bg-binary"></div>
-				<div className="window">
-					<img src="decor/window_n.png" alt="" className="popup normal" />
-					<img src="decor/window_c.gif" alt="" className='popup corrupt' />
-				</div>
-				<div className="niks">
-					<img src="/art/nik-art-center.png" alt="" className='nik-center' />
-					<img src="decor/frame-main.png" alt="" className='frame-main' />
-				</div>
+import "react";
+import SectionTitle from "@/app/components/SectionTitle/SectionTitle";
+import "./heroSection.scss";
+import Link from "next/link";
+export default function HeroSection({
+  bad,
+  good,
+  activeNav,
+}: {
+  bad?: number;
+  good?: number;
+  activeNav: string;
+}) {
+  return (
+    <section id="container_hero">
+      <h1 style={{ display: "none" }}>Nikulas Wraith</h1>
+      <div className="path-counter">
+        {bad && good && (
+          <p>
+            {" "}
+            {">>>"} <span className="good">{good}</span>-
+            <span className="bad">{bad}</span>
+            {"<<<"}
+          </p>
+        )}
+      </div>
+      <div className="display" id="top">
+        <div className="bg-binary"></div>
+        <div className="window">
+          <img src="decor/window_n.png" alt="" className="popup normal" />
+          <img src="decor/window_c.gif" alt="" className="popup corrupt" />
+        </div>
+        <div className="niks">
+          <img
+            src="/art/nik-art-center-new.png"
+            alt=""
+            className="nik-center"
+          />
+          <img src="decor/frame-main.png" alt="" className="frame-main" />
+        </div>
 
+        <div className="logo-container">
+          <img src="decor/main_logo.png" alt="" className="main-logo" />
+        </div>
+        <div className="header-frame">
+          <img src="/decor/header_l.png" alt="" />
+          <img src="/decor/header_r.png" alt="" />
+        </div>
 
-				<div className="logo-container">
-					<img src="decor/main_logo.png" alt="" className='main-logo' />
-				</div>
-				<div className="header-frame">
-					<img src="/decor/header_l.png" alt="" />
-					<img src="/decor/header_r.png" alt="" />
-				</div>
+        <div className="connection">
+          <img src="decor/top_connect_l.png" alt="" className="line l" />
+          <img src="decor/top_connect_r.png" alt="" className="line r" />
+        </div>
 
-			
-				<div className="connection">
-					<img src="decor/top_connect_l.png" alt="" className="line l" />
-					<img src="decor/top_connect_r.png" alt="" className='line r' />
-				</div>
+        <div className="arrows">
+          <img src="decor/arrow_small.png" alt="" className="arrow l" />
+          <img src="decor/arrow_small.png" alt="" className="arrow r" />
+        </div>
+      </div>
 
-				<div className="arrows">
-					<img src="decor/arrow_small.png" alt="" className='arrow l' />
-					<img src="decor/arrow_small.png" alt="" className='arrow r' />
-				</div>
-			</div>
-			
-			<header className='header'>
-				<div className="decor-stripe">
-					
-				</div>
-				<SectionTitle
-				contained={true}
-					directory='C:/Users/NikulasWraith/Project_Phantom/Nik.exe'
-					run={(<> {'run >>'} <b>navigation</b>.bat </>)}
-				/>
-				<nav className='navigation'>
-						<Link href="/?skip=1#about"  className={`${activeNav === 'home' ? 'selected' : ''}`}>About</Link> ||
-						<Link href="/?skip=1#showcase"  className={`${activeNav === '' ? 'selected' : ''}`}>Showcase</Link> ||
-						<Link href="/?skip=1#schedules"  className={`${activeNav === '' ? 'selected' : ''}`}>Schedules</Link> ||
-						<Link href="/?skip=1#artwork"  className={`${activeNav === '' ? 'selected' : ''}`}>Artwork</Link> ||
-						<Link href="/?skip=1#setup"  className={`${activeNav === '' ? 'selected' : ''}`}>Setup</Link> ||
-						<Link href="/?skip=1#socials"  className={`${activeNav === '' ? 'selected' : ''}`}>Socials</Link> ||
-						<Link href="/stream?skip=1" className={`${activeNav === 'stream' ? 'selected' : ''}`}>Stream</Link> 
-				</nav>
-			</header>
-		</section>
-	)
+      <header className="header">
+        <div className="decor-stripe"></div>
+        <SectionTitle
+          contained={true}
+          directory="C:/Users/NikulasWraith/Project_Phantom/Nik.exe"
+          run={
+            <>
+              {" "}
+              {"run >>"} <b>navigation</b>.bat{" "}
+            </>
+          }
+        />
+        <nav className="navigation">
+          <Link
+            href="/?skip=1#about"
+            className={`${activeNav === "home" ? "selected" : ""}`}
+          >
+            About
+          </Link>{" "}
+          ||
+          <Link
+            href="/?skip=1#showcase"
+            className={`${activeNav === "" ? "selected" : ""}`}
+          >
+            Showcase
+          </Link>{" "}
+          ||
+          <Link
+            href="/?skip=1#schedules"
+            className={`${activeNav === "" ? "selected" : ""}`}
+          >
+            Schedules
+          </Link>{" "}
+          ||
+          <Link
+            href="/?skip=1#artwork"
+            className={`${activeNav === "" ? "selected" : ""}`}
+          >
+            Artwork
+          </Link>{" "}
+          ||
+          <Link
+            href="/?skip=1#setup"
+            className={`${activeNav === "" ? "selected" : ""}`}
+          >
+            Setup
+          </Link>{" "}
+          ||
+          <Link
+            href="/?skip=1#socials"
+            className={`${activeNav === "" ? "selected" : ""}`}
+          >
+            Socials
+          </Link>{" "}
+          ||
+          <Link
+            href="/stream?skip=1"
+            className={`${activeNav === "stream" ? "selected" : ""}`}
+          >
+            Stream
+          </Link>
+        </nav>
+      </header>
+    </section>
+  );
 }
